dfe4056c5607badf8b5e46fdc137b0705bf504f4
Harrison VanderZee <harrison.vanderzee2@gmail.com>
2025-11-12 18:52:39 -0500

    Theming implemented

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,4 +8,20 @@ export default createVuetify({
   components,
   directives,
   // You can add theme, icons, etc. here
+
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: "#E85A4F",
+          secondary: "#E98074",
+          background: "#D8C3A5",
+          accent: "#8E8D8A",
+          // cardColor should be a valid hex string (include leading #)
+          cardColor: "#EAE7DC",
+        },
+      },
+    },
+  },
 });
