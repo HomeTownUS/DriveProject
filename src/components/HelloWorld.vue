dbc9ecdca5f4ec114421f6a6d70ac21371588947
Alex Reiffer <awr7648@gmail.com>
2025-12-10 13:44:37 -0500

    Fixed issue where logs displayed after sign out

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -156,6 +156,7 @@ export default defineComponent({
         } else {
           console.log("No user is signed in.");
           unsubscribe.value = null;
+          logs.value = [];
         }
       });
     });
